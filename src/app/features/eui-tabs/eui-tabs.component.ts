a76bab5c19d9af4c3afea0c5253c023b0adcd6d9
jonleb <jonathan.lebrun@outlook.be>
2021-11-20 11:21:53 +0100

    simple demo on add tabs

--- src/app/features/eui-tabs/eui-tabs.component.ts
+++ src/app/features/eui-tabs/eui-tabs.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { EuiTabComponent } from '@eui/components-next';
 
 @Component({
-    templateUrl: './eui-tabs.component.html'
+    templateUrl: './eui-tabs.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,    
 })
-export class EuiTabsComponent {
 
+export class EuiTabsComponent {
+    public userTabs = [];
+    
+    constructor(){};
+    
+    public addTab(){
+        // Do your logic here
+        this.userTabs.push(
+            {
+                tabLabel: 'User Tab', 
+                tabContent: 'User Tab content',
+                isDisabled: Math.random() >= 0.5,
+                isClosable: Math.random() >= 0.5 
+            });
+    }
 }
